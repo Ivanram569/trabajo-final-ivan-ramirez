880ba4483bfff5f5fea542358361b45804929cb3
Ivanram569 <108768862+Ivanram569@users.noreply.github.com>
2022-08-01 17:45:59 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function instagram(){
+    alert("La página de Instagram no está dsiponible, lo sentimos.")
+}
+
+function formulario(){
+    alert("El formulario se completo con éxito!, pronto nos comunicaremos con usted.")
+}
